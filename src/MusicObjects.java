d0658f7b9c7e4694da83cf3f1c0e959d62a99339
Ahto Türkson <ahto.turkson@estcube.eu>
2020-02-24 00:37:14 +0200

    First and last commit

---
+++ src/MusicObjects.java
@@ -0,0 +1,17 @@
+package player;
+
+//Iga laul on eraldi objektina
+public class MusicObjects {
+	
+	String path;
+	String name;
+	
+	public MusicObjects(String p, String n){
+		path = p;
+		name = n;
+	}
+	
+	
+	
+	
+}
